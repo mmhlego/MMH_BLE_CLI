8b60a42b9856a311b97756ce9f7ed05d7a15b7d5
mmhlego <mmhlego@gmail.com>
2022-08-02 00:40:09 +0430

    Project Initialized Added version and quit commands

---
+++ MMH_BLE_CLI/Helpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.UI.Xaml;
+
+namespace MMH_BLE_CLI
+{
+    class Helpers
+    {
+        static string VERSION = "alpha";
+        public static void PrintVersion() { //TODO
+            Console.WriteLine($"Current version: {VERSION}\n");
+        }
+
+    }
+}
---
+++ MMH_BLE_CLI/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth.Advertisement;
+
+namespace MMH_BLE_CLI
+{
+    class Program
+    {
+        BluetoothLEAdvertisementWatcher BleWatcher = new BluetoothLEAdvertisementWatcher
+        {
+            ScanningMode = BluetoothLEScanningMode.Active
+        };
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                string[] commands = Console.ReadLine().Split();
+                
+                if (commands.Length == 0)
+                {
+                    continue;
+                }
+
+                switch (commands[0].ToLower())
+                {
+                    case "q":
+                    case "quit":    // Quit console app
+                        System.Environment.Exit(0);
+                        break;
+
+                    case "--help":    // TODO: Show Help
+
+                        break;
+
+                    case "--v":
+                    case "--version":    // Show app version
+                        Helpers.PrintVersion();
+                        break;
+
+                    default:
+                        break;
+                }
+
+
+                /*if (commands[0].ToLower() == "q" || commands[0].ToLower() == "quit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Unknown command. Use \"help\" to get list of commands.");
+                }*/
+            }
+        }
+    }
+}
